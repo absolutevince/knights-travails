3e37dd4c396356a8fd300bcc93fd79dabfc2fc7e
vince <187113297+absolutevince@users.noreply.github.com>
2024-11-26 18:41:41 +0800

    Extract the knights-travail from project-dsa folder

---
+++ main.js
@@ -0,0 +1,3 @@
+console.log({ "[3,3] -> [4, 3]": knightMoves([3, 3], [4, 3]) });
+console.log({ "[0,0] -> [7, 7]": knightMoves([0, 0], [7, 7]) });
+console.log({ "[2,1] -> [0, 7]": knightMoves([0, 0], [7, 7]) });
